4d4c564ecea0acb6e3b9871f8c15a48cebe2a6b8
Djuri Baars <dsbaars@users.noreply.github.com>
2022-02-21 23:19:31 +0100

    Bootstrap v5 upgrade

--- src/app/shared/notification/toast/toast.component.ts
+++ src/app/shared/notification/toast/toast.component.ts
@@ -6,9 +6,17 @@ import { ToastService } from './toast.service';
   selector: 'app-toast',
   templateUrl: './toast.component.html',
   styleUrls: ['./toast.component.scss'],
+//  host: {'class': 'toast-container position-fixed top-0 end-0 p-3', 'style': 'z-index: 1200'}
 })
 export class ToastComponent {
-  @HostBinding('class.ngb-toasts')
+  @HostBinding('class.toast-container')
+  @HostBinding('class.position-fixed')
+  @HostBinding('class.top-0')
+  @HostBinding('class.end-0')
+  @HostBinding('class.p-3')
+  @HostBinding('style.z-index')
+  zIndex = 1200;
+
   readonly ngbToasts = true;
 
   constructor(public toastService: ToastService) {}
